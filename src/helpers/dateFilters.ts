f17bd97baa6cf8ab70a8e41d12c4203ca9bad726
Caio F <silvacaio295@gmail.com>
2022-01-21 16:47:05 -0300

    finalizar projeto

--- src/helpers/dateFilters.ts
+++ src/helpers/dateFilters.ts
@@ -11,7 +11,7 @@ export const filterListByMonth = (list: Item[], date:string): Item[]  => {
 
    for( let i in list) {
      if(
-      list[i].date.getFullYear() === parseInt(year) &&
+      list[i].date.getFullYear() === parseInt(year)  &&
       (list[i].date.getMonth()) === parseInt(month)
      ){
        newList.push(list[i])
@@ -31,3 +31,9 @@ export const formatDate = (date: Date): string => {
 }
 
 const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`  
+
+export const formatCurrentMonth = (currentMonth: string): string => {
+  let [year, month] = currentMonth.split('-')
+  let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro','Novembro', 'Dezembro']
+  return `${months[parseInt(month)]} de ${year}`
+}
